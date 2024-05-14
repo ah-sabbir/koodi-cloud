2a9195d18baa89fedea455c2ba4615d49b168b78
sabbir ahmmed <ahsabbir103@gmail.com>
2024-05-14 17:02:17 +0600

    resolve error

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="container mx-auto my-auto">
-      <span className=" font-recoleta font-semibold text-8xl text-white">We're here to outperform</span><br/>
+      <span className=" font-recoleta font-semibold text-8xl text-white">We&apos;re here to outperform</span><br/>
       <span className="animate-text-bottom font-recoleta font-semibold text-8xl text-white">performance agencies</span>
     </main>
   );
